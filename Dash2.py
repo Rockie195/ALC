8593c3459e122399204665348c8ef45459b68ab7
Rockie195 <24723359+Rockie195@users.noreply.github.com>
2021-03-18 15:50:05 -0700

    Remove multiple login instances

--- Dash2.py
+++ Dash2.py
@@ -7,20 +7,25 @@ from selenium.webdriver.support.ui import WebDriverWait as wait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 
-print('Type your Destiny password: ')
+print('Type your Destiny password: ', end='')
 password = pyinputplus.inputPassword()
 
-def enrollment_numbers(password, css_selector, program):
-    browser = webdriver.Firefox()
-    browser.get('https://uclasv.destinysolutions.com/srs/logon.do?method=logoff&firstTime=yes')
-    # Input username
-    wait(browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#loginId"))).send_keys('cjgomez')
-    # Input password
-    wait(browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#password"))).send_keys(password)
-    # Click logon button
-    wait(browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#logonButton"))).click()
-    # Click the programs tab
-    wait(browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#menu-link-CurrMgrPrograms"))).click()
+browser = webdriver.Firefox()
+browser.get('https://uclasv.destinysolutions.com/srs/logon.do?method=logoff&firstTime=yes')
+# Input username
+wait(browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#loginId"))).send_keys('cjgomez')
+# Input password
+wait(browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#password"))).send_keys(password)
+# Click logon button
+wait(browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#logonButton"))).click()
+
+def enrollment_numbers(css_selector, program):
+    # Click the programs tab, if something obscures it, wait for the obscuring element to disappear
+    try:
+        wait(browser, 20).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#menu-link-CurrMgrPrograms"))).click()
+    except:
+        wait(browser, 20).until(EC.invisibility_of_element_located((By.CSS_SELECTOR, "#lui_programOfferingPlacementGrid")))
+        wait(browser, 20).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#menu-link-CurrMgrPrograms"))).click()
     # Enter the Program name
     wait(browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#programName"))).send_keys(program)
     program_name = browser.find_element_by_css_selector('#programName')
@@ -38,24 +43,23 @@ def enrollment_numbers(password, css_selector, program):
     # Click on dashboard
     wait(browser, 10).until(EC.element_to_be_clickable(
         (By.CSS_SELECTOR, "#menu-link-CurrMgrProgramsProgramProfileDashboard"))).click()
+    hover.reset_actions()
     # If the dashboard appears, get the data. If there is no dashboard then there are zero students
     try:
         enrolled = browser.find_element_by_css_selector(
             '#genderSummary > tfoot:nth-child(2) > tr:nth-child(1) > td:nth-child(3)')
         number_enrolled = round(float(enrolled.text))
         print('\t The Dashboard number for ' + program_session_name + ' is ' + str(number_enrolled))
-        browser.quit()
     except NoSuchElementException:
         print('\t The Dashboard number for ' + program_session_name + ' is 0')
-        browser.quit()
-        return
+    return
 
 def iep_numbers(start, number_of_sessions, skip, program):
     program_values = {'AIEP': 1, 'IECP': 2, 'ACC': 3}
     for i in range(start, start + number_of_sessions):
         if i in skip:
             continue
-        enrollment_numbers(password, '#programOfferingProfile_PR000' + str(program_values[program]) + '_0'
+        enrollment_numbers('#programOfferingProfile_PR000' + str(program_values[program]) + '_0'
                            + str(i) + '_name', program)
 
 
@@ -70,3 +74,5 @@ print('Upcoming Numbers: ')
 iep_numbers(25, 1, [], 'AIEP')
 iep_numbers(62, 4, [], 'IECP')
 iep_numbers(50, 3, [], 'ACC')
+
+browser.quit()
